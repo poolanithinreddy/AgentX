3e973725aa3a5685243d2e65ff83192382c7ff45
Nithin Reddy <npoola1@umbc.edu>
2025-04-27 03:37:46 -0400

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -15,7 +15,7 @@ export default function Signup() {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/api/signup', {
+      const res = await fetch('https://agentx-backend-gyhjh6epgyafftey.canadacentral-01.azurewebsites.net/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, password }),
